224a197d368cbf57f2db65db3f7e954f550cdc82
Tobias Löpfe <tobias.loepfe@students.fhnw.ch>
2021-10-09 23:58:04 +0200

    creating table from JSON String in JS possible

--- frontend/main.js
+++ frontend/main.js
@@ -3,20 +3,42 @@ fetch(url).then(response => response.json())
 .then(data => console.log(data));
 
 
-let data = [{title: ":)", description: "=D"}, {title: "First", description: "Test"}]
-
+let json_string = '{"data": [{"title": ":)","description": "=D"}, {"title": "First","description": "Test"}]}'
+let data_from_api = JSON.parse(json_string)
+console.log(data_from_api.data)
 function generateTableHead(table, data) {
+    let labels = Object.keys(data.data[0])
+
     let thead = table.createTHead();
     let row = thead.insertRow();
-    console.log(data)
-    for (let key of data){
+    console.log(labels)
+    for (let label of labels){
         let th = document.createElement('th');
-        let text = document.createTextNode(key);
+        let text = document.createTextNode(label);
         th.appendChild(text);
         row.appendChild(th);
     }
+    
+    for(let i = 0; i<data.data.length; i++){
+        let tr = document.createElement('tr');
+        for(let label of labels){
+            let td = document.createElement('td');
+            let text = document.createTextNode(data.data[i][label]);
+            td.appendChild(text);
+            tr.appendChild(td);
+        }
+        table.appendChild(tr);
+    }
+
+    
+    
+    
+    
+    
+    
+    
+
   }
 
   let table = document.querySelector("table");
-  let keys = Object.keys(data[0]);
-  generateTableHead(table, keys);
+  generateTableHead(table, data_from_api);
