b99366ea3c45b15d0668b6f0b747efd77703aea6
Tobias Löpfe <tobias.loepfe@students.fhnw.ch>
2021-10-17 22:32:45 +0200

    deleted tmp files

--- backend/src/django_backend/django_backend/urls.py
+++ backend/src/django_backend/django_backend/urls.py
@@ -22,4 +22,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('testapp.urls')),
+    path('api/', include('jugglingapi.urls'))
 ]
